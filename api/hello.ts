ac773a1c827a93736cf6d441f16b318c08a89678
Michael Seifarth <michael@bynder.com>
2019-11-21 18:08:22 +0100

    build api for netlify

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,8 @@
 import { NowRequest, NowResponse } from "@now/node";
 
-export default (request: NowRequest, response: NowResponse) => {
+export const handler = (request: NowRequest, response: NowResponse) => {
   const { name = "World" } = request.query;
   response.status(200).send(`Hello ${name}!`);
 };
+
+export default handler;
