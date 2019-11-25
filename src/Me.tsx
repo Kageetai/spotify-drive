d2787f393537c04b93331b397e4abe94d8afd5a7
Michael Seifarth <michael@bynder.com>
2019-11-25 18:46:30 +0100

    more user info and some styles

--- src/Me.tsx
+++ src/Me.tsx
@@ -4,12 +4,23 @@ import spotifyApi from './utils/spotify';
 
 const Me: React.FC = () => {
   const [me, setMe] = React.useState<SpotifyApi.CurrentUsersProfileResponse>();
+  console.log(me);
 
   React.useEffect(() => {
     spotifyApi.getMe().then((res) => setMe(res.body));
   }, []);
 
-  return me ? <span>{me.display_name}</span> : null;
+  return me ? (
+    <div style={{ textTransform: 'capitalize' }}>
+      <p>{me.images && <img src={me.images[0].url} alt="" />}</p>
+
+      <a href={me.uri}>{me.display_name}</a>
+
+      <p>
+        {me.product}, {me.followers && me.followers.total} Followers
+      </p>
+    </div>
+  ) : null;
 };
 
 export default Me;
