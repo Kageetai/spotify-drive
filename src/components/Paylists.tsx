b1c37bd764a237251bcb5569fc086973b61aacf9
Michael Seifarth <michael@bynder.com>
2019-12-08 23:42:00 +0100

    Playlist component

--- src/components/Paylists.tsx
+++ src/components/Paylists.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import StyledPlaylists from '../styled/Playlists';
-
 import { useStoreActions, useStoreState } from '../store';
+
+import Playlist from './Playlist';
 
 const Playlists: React.FC = () => {
   const playlists = useStoreState((state) => state.playlists);
+  const selectedPlaylist = useStoreState((state) => state.selectedPlaylist);
   const fetchPlaylists = useStoreActions((actions) => actions.fetchPlaylists);
   const setSelectedPlaylist = useStoreActions(
     (actions) => actions.setSelectedPlaylist,
@@ -16,13 +18,20 @@ const Playlists: React.FC = () => {
   }, [fetchPlaylists]);
 
   return playlists ? (
-    <StyledPlaylists>
-      {playlists.map((playlist) => (
-        <li key={playlist.id} onClick={() => setSelectedPlaylist(playlist.id)}>
-          {playlist.name}
-        </li>
-      ))}
-    </StyledPlaylists>
+    selectedPlaylist ? (
+      <Playlist />
+    ) : (
+      <StyledPlaylists>
+        {playlists.map((playlist) => (
+          <li
+            key={playlist.id}
+            onClick={() => setSelectedPlaylist(playlist.id)}
+          >
+            {playlist.name}
+          </li>
+        ))}
+      </StyledPlaylists>
+    )
   ) : null;
 };
 
---
+++ src/components/Playlist.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Playlist: React.FC = () => {
+  return <span>Hello Playlist</span>;
+};
+
+export default Playlist;
