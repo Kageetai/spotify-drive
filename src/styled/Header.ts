fd41fe74a84859a06bd09b2750df5ddb855937c7
Michael Seifarth <michael@bynder.com>
2019-12-09 10:01:35 +0100

    lightgray theme color

--- src/styled/Header.ts
+++ src/styled/Header.ts
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
 
+import theme from './theme';
+
 export default styled('header')`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  background-color: lightgrey;
+  background-color: ${theme.colors.lightgray};
   padding: 0 1em;
 
   > h1 {
--- src/styled/Playlists.ts
+++ src/styled/Playlists.ts
@@ -1,17 +1,19 @@
 import styled from '@emotion/styled';
 
+import theme from './theme';
+
 export default styled('ul')`
   list-style: none;
   padding: 0;
   margin: 0;
 
   > li {
-    border-bottom: 1px solid lightgrey;
+    border-bottom: 1px solid ${theme.colors.lightgray};
     padding: 0.5em;
 
     &:hover {
       cursor: pointer;
-      background-color: lightgrey;
+      background-color: ${theme.colors.lightgray};
     }
   }
 `;
